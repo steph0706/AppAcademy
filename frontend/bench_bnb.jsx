6e5bc6d1145a2602d1e6bd9a1acd2cf2b0cf64ed
Scott Duane <scott.duane@liveramp.com>
2018-06-12 09:39:02 -0700

    Fix some webpack config stuff

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 import Root from './components/root';
 import configureStore from './store/store';
-
-    console.log("he");
 
 document.addEventListener('DOMContentLoaded', () => {
     const store = configureStore();
---
+++ frontend/components/root.jsx
@@ -0,0 +1,7 @@
+const Root = React.createComponent({
+  render () {
+    return <div>Hello there</div>;
+  }
+});
+
+export default Root;
